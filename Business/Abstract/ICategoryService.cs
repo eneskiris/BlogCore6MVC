486e25a1acd922fc792ed2966f27d3aa0b35dfa9
Enes <eneskiris@gmail.com>
2022-09-05 13:07:43 +0300

    #17 Asp.Net Core 5.0 Proje Kampı Business Katmanı Service ve Managerların Yazılması

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,12 @@
+using Entities.Concrete;
+
+namespace Business.Abstract;
+
+public interface ICategoryService
+{
+    void CategoryAdd(Category category);
+    void CategoryDelete(Category category);
+    void CategoryUpdate(Category category);
+    List<Category> GetList();
+    Category GetById(int id);
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete;
+
+public class CategoryManager : ICategoryService
+{
+    public void CategoryAdd(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CategoryDelete(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CategoryUpdate(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Category> GetList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Category GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
