708cd8ee6891ad83535c9ceda244801dcb570924
Enes <eneskiris@gmail.com>
2022-09-05 10:26:32 +0300

    #11 Asp.Net Core 5.0 Proje Kampı Referansların Tanımlanması Ve Context Dbset Propertyleri

---
+++ DataAccess/Concrete/BlogDemoContext.cs
@@ -0,0 +1,19 @@
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete;
+
+public class BlogDemoContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=BlogDemo;User Id=postgres;Password=enes35;");
+    }
+
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Writer> Writers { get; set; }
+}
