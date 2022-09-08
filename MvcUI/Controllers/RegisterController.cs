4df5146d6336933102afe8626d3018de1a3d82bc
Enes <eneskiris@gmail.com>
2022-09-08 21:10:53 +0300

    #34 Asp.Net Core 6.0 Author Registration and Use of Sweet Alert

--- MvcUI/Controllers/RegisterController.cs
+++ MvcUI/Controllers/RegisterController.cs
@@ -1,3 +1,5 @@
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,14 +7,20 @@ namespace MvcUI.Controllers;
 
 public class RegisterController : Controller
 {
+    WriterManager _writerManager = new WriterManager(new EfWriterRepository());
+
     [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
+
     [HttpPost]
     public IActionResult Index(Writer writer)
     {
+        writer.Status = true;
+        writer.About = "Test";
+        _writerManager.WriterAdd(writer);
         return RedirectToAction("Index", "Blog");
     }
 }
