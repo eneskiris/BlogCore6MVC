a9c6ec4f10d50c234dc20895f6c01334fdfe94f3
Enes <eneskiris@gmail.com>
2022-09-12 12:38:41 +0300

    #56 Asp.Net Core 6.0 Pulling Categories from Dropdown Tool on Adding Blog

--- MvcUI/Controllers/BlogController.cs
+++ MvcUI/Controllers/BlogController.cs
@@ -5,12 +5,15 @@ using Entities.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MvcUI.Controllers;
+
 [AllowAnonymous]
 public class BlogController : Controller
 {
     private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
     public IActionResult Index()
     {
         var values = _blogManager.GetListWithCategory();
@@ -23,17 +26,27 @@ public class BlogController : Controller
         var values = _blogManager.GetBlogListById(id);
         return View(values);
     }
-    
+
     public IActionResult BlogListByWriter()
     {
         var values = _blogManager.GetBlogByWriter(1);
         return View(values);
     }
+
     [HttpGet]
     public IActionResult BlogAdd()
     {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        List<SelectListItem> categoryvalues = (from x in _categoryManager.GetList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryId.ToString()
+                                               }).ToList();
+        ViewBag.CategoryValues = categoryvalues;
         return View();
-    }    
+    }
+
     [HttpPost]
     public IActionResult BlogAdd(Blog blog)
     {
