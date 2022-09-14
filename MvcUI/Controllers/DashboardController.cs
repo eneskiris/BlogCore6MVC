3c31e9227ba0cb91fbedc5e9d7603f4227480bf9
Enes <eneskiris@gmail.com>
2022-09-14 08:53:13 +0300

    #61 Asp.Net Core 6.0 Dashboard Preparation for the Author

---
+++ MvcUI/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.Controllers;
+
+public class DashboardController : Controller
+{
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
