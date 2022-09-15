15908ed284aaeaa10a7d3d3513d62f2ab09dd757
Enes <eneskiris@gmail.com>
2022-09-15 09:04:31 +0300

    #70 Asp.Net Core 6.0 Dashboard Statistics

--- MvcUI/Controllers/DashboardController.cs
+++ MvcUI/Controllers/DashboardController.cs
@@ -1,3 +1,4 @@
+using DataAccess.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,11 @@ public class DashboardController : Controller
     [AllowAnonymous]
     public IActionResult Index()
     {
+        BlogDemoContext context = new BlogDemoContext();
+        ViewBag.BlogsCount = context.Blogs.Count();
+        ViewBag.WriterBlogsCount = context.Blogs.Count(x => x.WriterId == 1);
+        ViewBag.CategoriesCount = context.Categories.Count();
+        
         return View();
     }
 }
