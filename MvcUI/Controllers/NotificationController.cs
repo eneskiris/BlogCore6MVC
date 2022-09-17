1a1b74395a1613c6ad553c6f268d7baf26477ffd
Enes <eneskiris@gmail.com>
2022-09-17 09:19:49 +0300

    #79 Asp.Net Core 6.0 Listing of Notifications

--- MvcUI/Controllers/NotificationController.cs
+++ MvcUI/Controllers/NotificationController.cs
@@ -1,12 +1,22 @@
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcUI.Controllers;
 
 public class NotificationController : Controller
 {
-    // GET
-    // public IActionResult Index()
-    // {
-    //     return View();
-    // }
+    NotificationManager _notificationManager = new NotificationManager(new EfNotificationRepository());
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [AllowAnonymous]
+    public IActionResult AllNotifications()
+    {
+        var values = _notificationManager.GetList();
+        return View(values);
+    }
 }
