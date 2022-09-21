6d1fe41fc460db9f52d2cc3b3ad1daaf9054fafc
Enes <eneskiris@gmail.com>
2022-09-21 13:37:33 +0300

    #106 Asp.Net Core 6.0 Listing Data in Console with Ajax

---
+++ MvcUI/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcUI.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace MvcUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class WriterController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult WriterList()
+    {
+        var jsonWriters = JsonConvert.SerializeObject(writers);
+        return Json(jsonWriters);
+    }
+    
+    public static List<WriterModel> writers = new List<WriterModel>
+    {
+        new WriterModel
+        {
+            Id = 1,
+            FullName = "Ahmet Yılmaz",
+        },
+        new WriterModel
+        {
+            Id = 2,
+            FullName = "Mehmet Bal",
+        },
+        new WriterModel
+        {
+            Id = 3,
+            FullName = "Ayşe Sönmez",
+        },
+    };
+}
---
+++ MvcUI/Areas/Admin/Models/WriterModel.cs
@@ -0,0 +1,7 @@
+namespace MvcUI.Areas.Admin.Models;
+
+public class WriterModel
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+}
