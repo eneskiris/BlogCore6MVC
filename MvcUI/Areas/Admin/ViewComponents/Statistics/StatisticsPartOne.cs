f3063ae42f18f69ce7a88ca4f4896bfa721e3252
Enes <eneskiris@gmail.com>
2022-09-21 10:00:51 +0300

    #103 Asp.Net Core 6.0 Fetch Weather Information Via API

--- MvcUI/Areas/Admin/ViewComponents/Statistics/StatisticsPartOne.cs
+++ MvcUI/Areas/Admin/ViewComponents/Statistics/StatisticsPartOne.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,13 @@ public class StatisticsPartOne : ViewComponent
         ViewBag.BlogsCount = _blogService.GetList().Count();
         ViewBag.MessagesCount = _messageService.GetList().Count();
         ViewBag.CommentCount = _commentService.GetList().Count();
+        
+        string apiKey = "55bae1403626b66bc18e2e5cd5af55ab";
+        string connection = "https://api.openweathermap.org/data/2.5/weather?q=izmir&mode=xml&units=metric&appid=" + apiKey;
+        XDocument document = XDocument.Load(connection);
+        
+        ViewBag.City= document.Descendants("city").Attributes("name").FirstOrDefault().Value;
+        ViewBag.Forecast = document.Descendants("temperature").Attributes("value").FirstOrDefault().Value;
         return View();
     }
 }
