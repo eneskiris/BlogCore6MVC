08a25be58f4e242965ca2ad28c65c135b26a1c07
Enes <eneskiris@gmail.com>
2022-09-28 10:20:26 +0300

    #136 Asp.Net Core 6.0 Admin Panel Sendbox and New Message Page

--- MvcUI/Areas/Admin/Controllers/AdminMessageController.cs
+++ MvcUI/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,3 +1,4 @@
+using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcUI.Areas.Admin.Controllers;
@@ -5,8 +6,30 @@ namespace MvcUI.Areas.Admin.Controllers;
 [Area("Admin")]
 public class AdminMessageController : Controller
 {
-    // GET
+    private IWriterService _writerService;
+    private IMessageWithWriterService _messageWithWriterService;
+
+    public AdminMessageController(IWriterService writerService, IMessageWithWriterService messageWithWriterService)
+    {
+        _writerService = writerService;
+        _messageWithWriterService = messageWithWriterService;
+    }
+
     public IActionResult Inbox()
+    {
+        var writerId = _writerService.GetWriterByEmail(User.Identity.Name).WriterId;
+        var values = _messageWithWriterService.GetInboxListByWriter(writerId);
+        return View(values);
+    }
+    
+    public IActionResult SendBox()
+    {
+        var writerId = _writerService.GetWriterByEmail(User.Identity.Name).WriterId;
+        var values = _messageWithWriterService.GetSendBoxListByWriter(writerId);
+        return View(values);
+    }
+
+    public IActionResult ComposeMessage()
     {
         return View();
     }
